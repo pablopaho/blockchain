f93c3d996afda60642e1111bafb4eefdd1c171e5
pablopaho <pablopaho@gmail.com>
2018-07-17 22:09:41 -0500

    add libraries

--- ethereum/insurance/compile.js
+++ ethereum/insurance/compile.js
@@ -2,7 +2,7 @@ const path = require('path');
 const fs = require('fs');
 const solc = require('solc');
 
-const inboxPath = path.resolve(__dirname, 'contracts', 'CommunityFactory.sol');
-const source = fs.readFileSync(inboxPath, 'utf8');
+const communityFactoryPath = path.resolve(__dirname, 'contracts', 'CommunityFactory.sol');
+const source = fs.readFileSync(communityFactoryPath, 'utf8');
 
 module.exports = solc.compile(source, 1).contracts[':CommunityFactory'];
